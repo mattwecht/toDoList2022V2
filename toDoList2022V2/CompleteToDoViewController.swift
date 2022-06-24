bbeec0d4065fd412d317f52686566873df1c2c8a
Matt Wecht <mattwecht@gmail.com>
2022-06-23 22:34:07 -0500

    Completed sample project

--- toDoList2022V2/CompleteToDoViewController.swift
+++ toDoList2022V2/CompleteToDoViewController.swift
@@ -9,18 +9,25 @@ import UIKit
 
 class CompleteToDoViewController: UIViewController {
     var previousVC = ToDoTableViewController()
-    var selectedToDo = ToDo()
+    //var selectedToDo = ToDo()
+    var selectedToDo : ToDoCD?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = selectedToDo.name
+        titleLabel.text = selectedToDo?.name
         // Do any additional setup after loading the view.
     }
     
     @IBOutlet weak var titleLabel: UILabel!
     
     @IBAction func completeTapped(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+          if let theToDo = selectedToDo {
+            context.delete(theToDo)
+            navigationController?.popViewController(animated: true)
+          }
         
     }
     
-}
+    }}
--- toDoList2022V2/ToDoTableViewController.swift
+++ toDoList2022V2/ToDoTableViewController.swift
@@ -9,26 +9,31 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
 
-    var toDos : [ToDo] = []
+    //var toDos : [ToDo] = []
+    var toDos : [ToDoCD] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        toDos = createToDos()
+        
+        //getToDos()
+        //toDos = createToDos()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+      getToDos()
     }
     
-    func createToDos() -> [ToDo] {
-
-      let walk = ToDo()
-        walk.name = "Walk Sammy before he throws a tantrum"
-        walk.important = true
-
-
-      let dog = ToDo()
-        dog.name = "Stare at Sammy"
-
-      return [walk, dog]
-    }
+//    func createToDos() -> [ToDo] {
+//
+//      let walk = ToDo()
+//        walk.name = "Walk Sammy before he throws a tantrum"
+//        walk.important = true
+//
+//
+//      let dog = ToDo()
+//        dog.name = "Stare at Sammy"
+//
+//      return [walk, dog]
+//    }
 
     // MARK: - Table view data source
 
@@ -43,17 +48,19 @@ class ToDoTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        
-        let toDo = toDos[indexPath.row]
-        
+      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+      let toDo = toDos[indexPath.row]
+
+      if let name = toDo.name {
         if toDo.important {
-          cell.textLabel?.text = "❗️" + toDo.name
+            cell.textLabel?.text = "❗️" + name
         } else {
-          cell.textLabel?.text = toDo.name
+            cell.textLabel?.text = toDo.name
         }
-        
-        return cell
+      }
+
+      return cell
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
       if let addVC = segue.destination as? AddToDoViewController {
@@ -61,7 +68,7 @@ class ToDoTableViewController: UITableViewController {
       }
 
       if let completeVC = segue.destination as? CompleteToDoViewController {
-        if let toDo = sender as? ToDo {
+          if let toDo = sender as? ToDoCD {
           completeVC.selectedToDo = toDo
           completeVC.previousVC = self
         }
@@ -76,39 +83,13 @@ class ToDoTableViewController: UITableViewController {
       performSegue(withIdentifier: "moveToComplete", sender: toDo)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    func getToDos() {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+
+        if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                toDos = coreDataToDos
+                tableView.reloadData()
+        }
+      }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
 }
